3e34b5681a558d8b3e3f75e41df192a61e40b8cc
Guillou Patrick <pitrackster@users.noreply.github.com>
2016-11-09 15:44:26 +0100

    [ExoBundle] Editor v2 open question (#1547)
    * [ExoBundle] Editor v2 open question
    
    * multiple changes

--- plugin/exo/Resources/modules/editor/items/open.js
+++ plugin/exo/Resources/modules/editor/items/open.js
@@ -1,32 +1,46 @@
 import {Open as component} from './open.jsx'
 import {ITEM_CREATE} from './../actions'
-import {update} from './../util'
-import {notBlank, number, gteZero} from './../lib/validate'
+import {setIfError, notBlank, number, gteZero, chain} from './../lib/validate'
+import {makeActionCreator} from './../util'
+import cloneDeep from 'lodash/cloneDeep'
+import merge from 'lodash/merge'
+import set from 'lodash/set'
+
+const UPDATE = 'UPDATE'
+
+export const actions = {
+  update: makeActionCreator(UPDATE, 'property', 'value')
+}
 
 function reduce(item = {}, action) {
   switch (action.type) {
     case ITEM_CREATE: {
-      return update(open, {
-        maxScore: {$set: 0},
-        maxLength: {$set: 0}
+      return Object.assign({}, item, {
+        maxScore: 0,
+        maxLength: 0
       })
+    }
+
+    case UPDATE: {
+      const newItem = cloneDeep(item)
+      newItem._touched = merge(
+        newItem._touched || {},
+        set({}, action.property, true)
+      )
+      newItem[action.property] = parseFloat(action.value)
+      return newItem
     }
   }
   return item
 }
 
-// function initialFormValues(item) {
-//   return update(item, {
-//     maxScore: {$set: item.score.max}
-//   })
-// }
 
 function validate(values) {
-  return {
-    maxScore: notBlank(values.maxScore)
-      || number(values.maxScore)
-      || gteZero(values.maxScore)
-  }
+  const errors = {}
+  setIfError(errors, 'maxScore', chain(values.maxScore, [notBlank, number, gteZero]))
+  setIfError(errors, 'maxLength', chain(values.maxLength, [notBlank, number, gteZero]))
+
+  return errors
 }
 
 export default {
