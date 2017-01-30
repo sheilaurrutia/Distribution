01cea845f5dabf25b8f2ba2f34fbdff1797f150f
kitan1982 <anhthao.pham@claroline.net>
2017-01-30 18:17:48 +0100

    Adds temp feedback page for pair question

--- plugin/exo/Resources/modules/items/pair/index.js
+++ plugin/exo/Resources/modules/items/pair/index.js
@@ -1,6 +1,7 @@
 import editor from './editor'
 import {PairPaper} from './paper.jsx'
 import {PairPlayer} from './player.jsx'
+import {PairFeedback} from './feedback.jsx'
 
 function expectAnswer() {
   return []
@@ -11,6 +12,7 @@ export default {
   name: 'pair',
   paper: PairPaper,
   player: PairPlayer,
+  feedback: PairFeedback,
   editor,
   expectAnswer
 }
