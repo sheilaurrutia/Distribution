5e9fefd97dbf8e7d755a596575580de84ec0ca3a
Stéphane Klein <stephaneklein221@gmail.com>
2017-01-31 17:16:27 +0100

    Fix save guard (#1909)

--- plugin/exo/Resources/modules/quiz/editor/save-guard.js
+++ plugin/exo/Resources/modules/quiz/editor/save-guard.js
@@ -2,12 +2,10 @@ import {tex} from './../../utils/translate'
 import select from './selectors'
 
 export function makeSaveGuard(getState) {
-  const state = getState()
-
   // no need to attach a listener if the quiz is readonly
-  if (select.editor(state)) {
+  if (select.editor(getState())) {
     window.addEventListener('beforeunload', e => {
-      if (!select.saved(state)) {
+      if (!select.saved(getState())) {
         // note: this is supposed to be the text displayed in the browser built-in
         // popup (see https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onbeforeunload#Example)
         // but it doesn't seem to be actually used in modern browsers. We use it
