f6d3e5d03b194e69ded892cf3c2c541ce4711997
ngodfraind <nicolas.godfraind@gmail.com>
2017-03-14 10:46:06 +0100

    Rebuild migrations

--- plugin/exo/Migrations/pdo_mysql/Version20170314104451.php
+++ plugin/exo/Migrations/pdo_mysql/Version20170314104451.php
@@ -8,18 +8,21 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated migration based on mapping information: modify it with caution
  *
- * Generation date: 2017/02/21 12:36:38
+ * Generation date: 2017/03/14 10:44:51
  */
-class Version20170221123637 extends AbstractMigration
+class Version20170314104451 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         $this->addSql("
-            CREATE TABLE ujm_color (
+            CREATE TABLE ujm_color_selection (
                 id INT AUTO_INCREMENT NOT NULL, 
-                score DOUBLE PRECISION NOT NULL, 
                 selection_id INT DEFAULT NULL, 
-                INDEX IDX_AADB06B4E48EFE78 (selection_id), 
+                color_id INT DEFAULT NULL, 
+                score DOUBLE PRECISION NOT NULL, 
+                feedback LONGTEXT DEFAULT NULL, 
+                INDEX IDX_97921969E48EFE78 (selection_id), 
+                INDEX IDX_979219697ADA1FB5 (color_id), 
                 PRIMARY KEY(id)
             ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB
         ");
@@ -38,13 +41,13 @@ class Version20170221123637 extends AbstractMigration
             ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB
         ");
         $this->addSql("
-            CREATE TABLE ujm_selection_color (
+            CREATE TABLE ujm_color (
                 id INT AUTO_INCREMENT NOT NULL, 
                 interaction_qcm_id INT DEFAULT NULL, 
                 colorCode VARCHAR(255) NOT NULL, 
                 uuid VARCHAR(36) NOT NULL, 
-                UNIQUE INDEX UNIQ_3CB7F80FD17F50A6 (uuid), 
-                INDEX IDX_3CB7F80F9DBF539 (interaction_qcm_id), 
+                UNIQUE INDEX UNIQ_AADB06B4D17F50A6 (uuid), 
+                INDEX IDX_AADB06B49DBF539 (interaction_qcm_id), 
                 PRIMARY KEY(id)
             ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB
         ");
@@ -55,12 +58,20 @@ class Version20170221123637 extends AbstractMigration
                 htmlWithoutValue LONGTEXT NOT NULL, 
                 mode VARCHAR(255) NOT NULL, 
                 tries INT NOT NULL, 
-                globalSuccessScore INT DEFAULT NULL, 
-                globalFailureScore INT DEFAULT NULL, 
                 penalty INT DEFAULT NULL, 
                 UNIQUE INDEX UNIQ_7B1E8B31E27F6BF (question_id), 
                 PRIMARY KEY(id)
             ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB
+        ");
+        $this->addSql("
+            ALTER TABLE ujm_color_selection 
+            ADD CONSTRAINT FK_97921969E48EFE78 FOREIGN KEY (selection_id) 
+            REFERENCES ujm_selection (id)
+        ");
+        $this->addSql("
+            ALTER TABLE ujm_color_selection 
+            ADD CONSTRAINT FK_979219697ADA1FB5 FOREIGN KEY (color_id) 
+            REFERENCES ujm_color (id)
         ");
         $this->addSql("
             ALTER TABLE ujm_selection 
@@ -68,8 +79,8 @@ class Version20170221123637 extends AbstractMigration
             REFERENCES ujm_interaction_selection (id)
         ");
         $this->addSql("
-            ALTER TABLE ujm_selection_color 
-            ADD CONSTRAINT FK_3CB7F80F9DBF539 FOREIGN KEY (interaction_qcm_id) 
+            ALTER TABLE ujm_color 
+            ADD CONSTRAINT FK_AADB06B49DBF539 FOREIGN KEY (interaction_qcm_id) 
             REFERENCES ujm_interaction_selection (id)
         ");
         $this->addSql("
@@ -90,21 +101,29 @@ class Version20170221123637 extends AbstractMigration
     public function down(Schema $schema)
     {
         $this->addSql("
+            ALTER TABLE ujm_color_selection 
+            DROP FOREIGN KEY FK_97921969E48EFE78
+        ");
+        $this->addSql("
+            ALTER TABLE ujm_color_selection 
+            DROP FOREIGN KEY FK_979219697ADA1FB5
+        ");
+        $this->addSql("
             ALTER TABLE ujm_selection 
             DROP FOREIGN KEY FK_C93913FF9DBF539
         ");
         $this->addSql("
-            ALTER TABLE ujm_selection_color 
-            DROP FOREIGN KEY FK_3CB7F80F9DBF539
+            ALTER TABLE ujm_color 
+            DROP FOREIGN KEY FK_AADB06B49DBF539
         ");
         $this->addSql("
-            DROP TABLE ujm_color
+            DROP TABLE ujm_color_selection
         ");
         $this->addSql("
             DROP TABLE ujm_selection
         ");
         $this->addSql("
-            DROP TABLE ujm_selection_color
+            DROP TABLE ujm_color
         ");
         $this->addSql("
             DROP TABLE ujm_interaction_selection
